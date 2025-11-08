ebe195b38ee9756d16c5818eda000d72c2f0f17c
wjma <wjma28@163.com>
2025-11-09 00:15:29 +0800

    11/9 github test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 import matplotlib.pyplot as plt
+import numpy as np
 
 from custom.stackedWidget import StackedWidget
 from custom.treeView import FileSystemTreeView
